984d767c05ce409d48feb6b2c98c013732027abf
Jonathan Carpenter <jonathan.carpenter98@gmail.com>
2020-11-22 18:36:46 +0000

    Refactor compelte, working on preventing invalid inputs

--- refactor_cell.py
+++ refactor_cell.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 class Cell:
     def __init__(self, pos, master, entry=None):
         self.entry = entry
@@ -11,13 +13,14 @@ class Cell:
             return str(self.entry)
 
     def __eq__(self, other):
+        if other == None or self.entry == None or other.entry == None: return False
         return self.entry == other.entry
 
     def isValid(self):
         r, c = self.pos[0], self.pos[1]
 
         exclusives = (self.master.getRow(r)
-                    + self.master.getCol(c)
+                    + self.master.getCol(r, c)
                     + self.master.getSquare(r, c))
 
         for cell in exclusives:
@@ -39,11 +42,27 @@ class Cell:
         return list(valids)
 
 class GUICell(Cell):
-    def __init__(self, widget=None, default_colour=None):
-        super().__init__()
+    def __init__(self, pos, master, widget=None, default_colour=None):
+        super().__init__(pos, master)
         self.widget = widget
 
-        if default_colour == None: default_colour = self.master.default_colour
+        if default_colour == None: self.default_colour = self.master.known_colour
         else: self.default_colour = default_colour
 
-        self.current_colour = default_colour
+        self.set_colour(default_colour)
+
+    def set_colour(self, colour):
+        if self.widget != None: self.widget["bg"] = colour
+
+    def set_entry(self, num):
+        self.widget["text"] = num
+        self.entry = int(float(num))
+
+        if self.isValid():
+            self.widget["bg"] = self.default_colour
+        else:
+            self.widget["bg"] = self.master.invalid_colour
+            sleep(0.1)
+            self.widget["bg"] = self.default_colour
+            self.widget["text"] = ""
+            self.entry = None
--- refactor_logic.py
+++ refactor_logic.py
@@ -1,7 +1,7 @@
-from refactor_cell import Cell, GUICell
 import refactor_cell
 
 import random
+import tkinter as tk
 
 class Sudoku:
     def __init__(self, difficulty=0.7):
@@ -18,8 +18,8 @@ class Sudoku:
             self.cells[r] = []
             for c in range(9):
                 # print("\nSetting cell {}...".format((r,c)))
-                if isinstance(self, Sudoku): cell = Cell([r,c], self)
-                else: cell = GUICell([r,c], self)
+                if isinstance(self, GUISudoku): cell = refactor_cell.GUICell([r,c], self)
+                else: cell = refactor_cell.Cell([r,c], self)
 
                 try:
                     cell.entry = random.choice(cell.getValids())
@@ -49,3 +49,48 @@ class Sudoku:
             for cell in row[c-c%3:c-c%3+3]:
                 square.append(cell)
         return square
+
+class GUISudoku(Sudoku):
+    def __init__(self, master, cell_size=3, known_colour="gray80", unknown_colour="gray95", updating_colour="sky blue", invalid_colour="tomato", **kwds):
+        self.master = master
+        self.cell_size = cell_size
+        self.known_colour = known_colour
+        self.unknown_colour = unknown_colour
+        self.updating_colour = updating_colour
+        self.invalid_colour = invalid_colour
+        self.active_cell = None
+        super().__init__(**kwds)
+
+        self.display()
+
+    def display(self):
+        for r in range(9):
+            for c in range(9):
+                self.print_cell(r, c)
+
+    def print_cell(self, r, c, pad_x=0, pad_y=0):
+        if r in {3,6}: pad_y = (self.cell_size, 0)
+        if c in {3,6}: pad_x = (self.cell_size, 0)
+
+        cell = self.cells[r][c]
+        cell.widget = tk.Label(self.master, text=str(cell),
+          width=self.cell_size*2, height=self.cell_size,
+          borderwidth=2, relief="groove")
+        cell.widget.grid(row=r, column=c, padx=pad_x, pady=pad_y)
+        cell.widget.bind("<Button-1>", lambda e: self.set_active_cell(cell))
+        cell.widget["bg"] = cell.default_colour
+
+    def set_active_cell(self, cell):
+        if cell.widget["bg"] == self.known_colour: return
+
+        if self.active_cell != None:
+            self.active_cell.set_colour(self.active_cell.default_colour)
+
+        self.active_cell = cell
+        cell.set_colour(self.updating_colour)
+
+    def active_cell_entry(self, event):
+        if self.active_cell == None or event.char not in "123456789":
+            return
+
+        self.active_cell.set_entry(event.char)
